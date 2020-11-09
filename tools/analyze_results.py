d727ebf50b632ca623556bee87c2ccc0d73a9c45
Christian Heimes <christian@python.org>
2020-11-09 17:14:19 +0100

    Analyze more

--- tools/analyze_results.py
+++ tools/analyze_results.py
@@ -6,12 +6,34 @@ import sys
 result = {}
 
 for filename in sorted(glob.glob("result*.json")):
+    name = filename[len("result-"):].rsplit(".", 1)[0]
+
     with open(filename) as f:
         j = json.load(f)
-    result[filename] = [
+
+    openssldirs = [
         key
-        for key, value in j["openssldir_candidates"].items()
+        for key, value in j["candidates_openssldir"].items()
         if value["valid"]
     ]
+    if j["default_verify"]["truststore"]:
+        dvp = dict(
+            cafile=j["default_verify"]["cafile"]["path"],
+            cafile_valid=bool(j["default_verify"]["cafile"]["certs"]),
+            capath=j["default_verify"]["capath"]["path"],
+            capath_valid=bool(j["default_verify"]["capath"]["certs"]),
+        )
+    else:
+        dvp = None
 
-json.dump(result, sys.stdout, indent=2)
+    ca_certs_count = j["default_context"]["ca_certs_count"]
+
+    result[name] = dict(
+        openssldirs=openssldirs,
+        default_verify_paths=dvp,
+        default_context_can_verify=bool(ca_certs_count),
+        default_context_ca_certs_count=ca_certs_count,
+        openssl_version=j["openssl"]["version"]
+    )
+
+json.dump(result, sys.stdout, indent=2, sort_keys=True)
